2822a93fd00304869f0cd39749b6977356bfb8fc
acesanderson <brian.briananderson@gmail.com>
2026-04-08 18:52:36 -0500

    test(ephemeral): verify image format sniffing for AC 4

--- siphon-client/tests/test_ephemeral.py
+++ siphon-client/tests/test_ephemeral.py
@@ -19,3 +19,21 @@ def test_sniff_bytes_plain_text_multiline():
     """AC 3 (partial): multiline UTF-8 text sniffs to .txt."""
     data = "Line one\nLine two\nLine three\n".encode("utf-8")
     assert sniff_bytes(data) == ".txt"
+
+
+def test_sniff_bytes_png():
+    """AC 4 (partial): PNG magic bytes sniff to .png."""
+    png_header = b"\x89PNG\r\n\x1a\n" + b"\x00" * 4
+    assert sniff_bytes(png_header) == ".png"
+
+
+def test_sniff_bytes_jpeg():
+    """AC 4 (partial): JPEG magic bytes sniff to .jpg."""
+    jpeg_header = b"\xff\xd8\xff" + b"\x00" * 9
+    assert sniff_bytes(jpeg_header) == ".jpg"
+
+
+def test_sniff_bytes_gif():
+    """AC 4 (partial): GIF magic bytes sniff to .gif."""
+    gif_header = b"GIF8" + b"\x00" * 8
+    assert sniff_bytes(gif_header) == ".gif"
