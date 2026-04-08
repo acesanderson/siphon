57b5faa7aa603e97f85bc3b76464a110569d8f5f
acesanderson <brian.briananderson@gmail.com>
2026-04-08 18:52:28 -0500

    test(ephemeral): verify plain text sniffing for AC 3

--- siphon-client/tests/test_ephemeral.py
+++ siphon-client/tests/test_ephemeral.py
@@ -8,3 +8,14 @@ def test_sniff_bytes_rejects_zip():
     zip_header = b"PK\x03\x04" + b"\x00" * 8
     with pytest.raises(EphemeralInputError, match="ZIP files are not supported"):
         sniff_bytes(zip_header)
+
+
+def test_sniff_bytes_plain_text_returns_txt():
+    """AC 3 (partial): plain UTF-8 text sniffs to .txt."""
+    assert sniff_bytes(b"hello world") == ".txt"
+
+
+def test_sniff_bytes_plain_text_multiline():
+    """AC 3 (partial): multiline UTF-8 text sniffs to .txt."""
+    data = "Line one\nLine two\nLine three\n".encode("utf-8")
+    assert sniff_bytes(data) == ".txt"
